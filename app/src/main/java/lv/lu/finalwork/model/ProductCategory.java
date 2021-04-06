a48fb84e54d79ae47a5404bda301715005b38f12
LinardsLaugalis <linards@lasma.eu>
2021-04-06 22:14:50 +0300

    move final work

---
+++ app/src/main/java/lv/lu/finalwork/model/ProductCategory.java
@@ -0,0 +1,5 @@
+package lv.lu.finalwork.model;
+
+public enum ProductCategory {
+    FRUIT, MEAT, FISH, MILK, BREAD, VEGETABLES, CANDY
+}
---
+++ app/src/main/java/lv/lu/finalwork/reository/Repository.java
@@ -0,0 +1,10 @@
+package lv.lu.finalwork.reository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    Long save(T entity);
+    List<T> findAll();
+    T findById(Long id);
+    void delete (Long id);
+}
